a6050fd29a56758b1f3f3de55515edb82f8070d4
Tobias Jacob <tobi.jacob@gmx.net>
2023-01-31 22:42:12 +0100

    Trained ngram model

---
+++ tinychatgpt/dataset.py
@@ -0,0 +1,24 @@
+#%%
+from datasets import load_dataset
+
+dataset = load_dataset("amazon_reviews_multi", "de")
+train = dataset["train"]
+
+#%%
+
+lines = []
+with open("dataset.txt", "w") as f:
+    for i, r in enumerate(train):
+        if len(r['review_body']) > 300:
+            lines.append(f"Stars: {r['stars']}; Review Body: {r['review_body']}\n")
+        if len(lines) > 30:
+            f.writelines(
+                lines
+            )
+            lines = []
+
+
+# %%
+import tiktoken
+enc = tiktoken.get_encoding("gpt2")
+assert enc.decode(enc.encode("hello world")) == "hello world"
---
+++ tinychatgpt/model.py
@@ -0,0 +1,130 @@
+#%%
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+with open("dataset.txt", "r") as f:
+    dataset = f.readlines()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+#%%
+allCharacters = set("".join(dataset))
+characters = set("\n !(),.0123456789qwertyuioplkjhgfdsazxcvbnmQWWERTYUIOPLKJHGFDSAZXCVBNMäöüÄÖÜß")
+characters.add("Stars: ")
+characters.add("; Review Body: ")
+
+# %%
+stoi = {char: i for i, char in enumerate(characters)}
+itos = {i: char for i, char in enumerate(characters)}
+n_tokens = len(characters)
+
+def encode(text):
+    return [stoi[char] for char in text if char in characters]
+
+def decode(text):
+    return "".join([itos[char] for char in text])
+
+tokenized_dataset = []
+for i, line in enumerate(dataset):
+    # Parse stars
+    stars = line.split("Stars: ")[1].split(";")[0]
+    # Parse review body
+    review = line.split("; Review Body: ")[1]
+
+    tokenized_dataset.append([stoi["Stars: "]] + encode(stars) + [stoi["; Review Body: "]] + encode(review))
+
+# shuffle dataset
+random.shuffle(tokenized_dataset)
+
+tokenized_dataset = [char for line in tokenized_dataset for char in line]
+
+# %%
+decode(tokenized_dataset[:1000])
+# %%
+
+train = tokenized_dataset[:int(len(tokenized_dataset) * 0.8)]
+test = tokenized_dataset[int(len(tokenized_dataset) * 0.8):]
+
+train = torch.tensor(train, dtype=torch.long).to(device)
+test = torch.tensor(test, dtype=torch.long).to(device)
+
+def get_batch(split, batch_size, seq_len):
+    start = torch.randint(0, len(split) - seq_len - 1, (batch_size,))
+    batch = torch.zeros((batch_size, seq_len), dtype=torch.long, device=device)
+    target = torch.zeros((batch_size), dtype=torch.long, device=device)
+    for i in range(batch_size):
+        batch[i] = split[start[i]:start[i] + seq_len]
+        target[i] = split[start[i] + seq_len]
+    return batch, target
+
+get_batch(train, 32, 10)
+# %%
+# Define ngram model
+class NgramModel(nn.Module):
+    def __init__(self, n_tokens: int, n_embed: int, n_hidden: int, seq_len: int):
+        super(NgramModel, self).__init__()
+        self.encoder = nn.Embedding(n_tokens, n_embed)
+        self.fc1 = nn.Linear(n_embed * seq_len, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_tokens)
+        self.decoder = nn.Linear(n_hidden, n_tokens)
+
+    def forward(self, x, y=None):
+        # x.shape = (batch_size, seq_len)
+        x = self.encoder(x)
+        # x.shape = (batch_size, seq_len, n_embed)
+        x = x.view(x.shape[0], -1)
+        # x.shape = (batch_size, seq_len * n_embed)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # x.shape = (batch_size, n_tokens)
+        if y is not None:
+            return F.cross_entropy(x, y)
+        return x
+
+    def generate(self, x, num_chars: int):
+        # x.shape = (batch_size, seq_len)
+        for i in range(num_chars):
+            x_next = self.forward(x[:, i:])
+            # x_next.shape = (batch_size, n_tokens)
+            # sample next character
+            x_next = torch.multinomial(F.softmax(x_next, dim=1), 1)
+            # x_next.shape = (batch_size, 1)
+            x = torch.cat((x, x_next), dim=1)
+        return x
+
+# train the model
+seq_len = 16
+batch_size = 32
+model = NgramModel(n_tokens, 256, 256, seq_len)
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+losses_train = []
+losses_test = []
+for i in range(600):
+    for split in [train, test]:
+        batch, target = get_batch(split, batch_size, seq_len)
+        loss = model(batch, target)
+        if split is train:
+            losses_train.append(loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        else:
+            losses_test.append(loss.item())
+        if i % 100 == 1:
+            print(f"Loss train: {losses_train[-1]} Loss test: {losses_test[-1]}")
+plt.plot(losses_train)
+plt.plot(losses_test)
+plt.show()
+# %%
+
+# generate text
+batch, target = get_batch(test, 1, seq_len)
+print(decode(model.generate(batch, 1000).squeeze().tolist()))
+
+# %%
