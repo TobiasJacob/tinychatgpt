854fea3afd4f5dbaa600fb1a8f2264bd44c0fdc8
TobiasJacob <tobi.jacob@gmx.net>
2023-02-01 14:46:51 +0100

    Refactored ngram model

---
+++ tinychatgpt/models/NgramModel.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class NgramModel(nn.Module):
+    def __init__(self, n_tokens: int, n_embed: int, n_hidden: int, seq_len: int):
+        super(NgramModel, self).__init__()
+        self.encoder = nn.Embedding(n_tokens, n_embed)
+        self.fc1 = nn.Linear(n_embed * seq_len, n_hidden)
+        self.fc2 = nn.Linear(n_hidden, n_hidden)
+        self.fc3 = nn.Linear(n_hidden, n_tokens)
+        self.decoder = nn.Linear(n_hidden, n_tokens)
+
+    def forward(self, x, y=None):
+        # x.shape = (batch_size, seq_len)
+        x = self.encoder(x)
+        # x.shape = (batch_size, seq_len, n_embed)
+        x = x.view(x.shape[0], -1)
+        # x.shape = (batch_size, seq_len * n_embed)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        # x.shape = (batch_size, n_tokens)
+        if y is not None:
+            return F.cross_entropy(x, y)
+        return x
+
+    def generate(self, x, num_chars: int):
+        # x.shape = (batch_size, seq_len)
+        for i in range(num_chars):
+            x_next = self.forward(x[:, i:])
+            # x_next.shape = (batch_size, n_tokens)
+            # sample next character
+            x_next = torch.multinomial(F.softmax(x_next, dim=1), 1)
+            # x_next.shape = (batch_size, 1)
+            x = torch.cat((x, x_next), dim=1)
+        return x
--- tinychatgpt/train.py
+++ tinychatgpt/train.py
@@ -1,8 +1,6 @@
 #%%
 import random
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 import sys
@@ -11,6 +9,7 @@ if ".." not in sys.path:
     sys.path.append("..")
 
 from tinychatgpt.dataset import N_TOKENS, encode, decode, load_dataset
+from tinychatgpt.models.NgramModel import NgramModel
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #%%
@@ -18,6 +17,7 @@ tokenized_dataset = load_dataset()
 
 # %%
 decode(tokenized_dataset[:1000])
+
 # %%
 train = tokenized_dataset[:int(len(tokenized_dataset) * 0.8)]
 test = tokenized_dataset[int(len(tokenized_dataset) * 0.8):]
@@ -36,45 +36,12 @@ def get_batch(split, batch_size, seq_len):
 
 get_batch(train, 32, 10)
 # %%
+
 # Define ngram model
-class NgramModel(nn.Module):
-    def __init__(self, n_tokens: int, n_embed: int, n_hidden: int, seq_len: int):
-        super(NgramModel, self).__init__()
-        self.encoder = nn.Embedding(n_tokens, n_embed)
-        self.fc1 = nn.Linear(n_embed * seq_len, n_hidden)
-        self.fc2 = nn.Linear(n_hidden, n_hidden)
-        self.fc3 = nn.Linear(n_hidden, n_tokens)
-        self.decoder = nn.Linear(n_hidden, n_tokens)
-
-    def forward(self, x, y=None):
-        # x.shape = (batch_size, seq_len)
-        x = self.encoder(x)
-        # x.shape = (batch_size, seq_len, n_embed)
-        x = x.view(x.shape[0], -1)
-        # x.shape = (batch_size, seq_len * n_embed)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        # x.shape = (batch_size, n_tokens)
-        if y is not None:
-            return F.cross_entropy(x, y)
-        return x
-
-    def generate(self, x, num_chars: int):
-        # x.shape = (batch_size, seq_len)
-        for i in range(num_chars):
-            x_next = self.forward(x[:, i:])
-            # x_next.shape = (batch_size, n_tokens)
-            # sample next character
-            x_next = torch.multinomial(F.softmax(x_next, dim=1), 1)
-            # x_next.shape = (batch_size, 1)
-            x = torch.cat((x, x_next), dim=1)
-        return x
-
 # train the model
 seq_len = 16
 batch_size = 32
-model = NgramModel(n_tokens, 256, 256, seq_len)
+model = NgramModel(N_TOKENS, 256, 256, seq_len)
 model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 losses_train = []
